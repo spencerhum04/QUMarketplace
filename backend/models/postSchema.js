dc0fe3bf6e440aef3b74136b36513b26a0107c62
spencerhum04 <spencerhum04@gmail.com>
2025-05-31 11:02:01 -0400

    feat: models

---
+++ backend/models/postSchema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    like: {
+        type: Array,
+        default: []
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User
+    },
+    bookmarks: {
+        type: Array,
+        default: []
+    },
+}, {timestamps: true})
+
+export const User = mongoose.model("Post", userSchema);
