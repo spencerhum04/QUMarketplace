47b7d1aa27273ffbc109970966c21a798e7cfd3f
spencerhum04 <spencerhum04@gmail.com>
2025-06-21 15:08:34 -0400

    fix: useState error

--- frontend/src/pages/signup/signup.tsx
+++ frontend/src/pages/signup/signup.tsx
@@ -1,32 +1,40 @@
 import { useState } from "react";
 import Navbar from "../../components/Navbar";
 
-const [login, setLogin] = useState(true);
-const [name, setName] = useState("");
-const [username, setUsername] = useState("");
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-
 export default function Register() {
+
+    const [login, setLogin] = useState(true);
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(name, username, email, password);
+    }
+
     return (
         <>
             <Navbar />
-            <div className="flex flex-col gap-y-2 w-40">
-                <label>Name</label>
-                <input type="text" value={name} className="bg-white text-black" />
-            </div>
-            <div className="flex flex-col gap-y-2 w-40">
-                <label>Username</label>
-                <input type="text" value={username} className="bg-white text-black" />
-            </div>
-            <div className="flex flex-col gap-y-2 w-40">
-                <label>Email address</label>
-                <input type="email" value={email} className="bg-white text-black" />
-            </div>
-            <div className="flex flex-col gap-y-2 w-40">
-                <label>Password</label>
-                <input type="password" value={password} className="bg-white text-black" />
-            </div>
+            <form>
+                <div className="flex flex-col gap-y-2 w-40">
+                    <label>Name</label>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="bg-white text-black" />
+                </div>
+                <div className="flex flex-col gap-y-2 w-40">
+                    <label>Username</label>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="bg-white text-black" />
+                </div>
+                <div className="flex flex-col gap-y-2 w-40">
+                    <label>Email address</label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-white text-black" />
+                </div>
+                <div className="flex flex-col gap-y-2 w-40">
+                    <label>Password</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="bg-white text-black" />
+                </div>
+            </form>
         </>
     )
 }
