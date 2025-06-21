ce122a6fb9eaf1d7ba7303da83ebbb3634b2f93c
spencerhum04 <spencerhum04@gmail.com>
2025-06-21 15:00:28 -0400

    feat: login values

--- frontend/src/pages/signup/signup.tsx
+++ frontend/src/pages/signup/signup.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
 import Navbar from "../../components/Navbar";
+
+const [login, setLogin] = useState(true);
+const [name, setName] = useState("");
+const [username, setUsername] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
 
 export default function Register() {
     return (
@@ -6,19 +13,19 @@ export default function Register() {
             <Navbar />
             <div className="flex flex-col gap-y-2 w-40">
                 <label>Name</label>
-                <input className="bg-white text-black" />
+                <input type="text" value={name} className="bg-white text-black" />
             </div>
             <div className="flex flex-col gap-y-2 w-40">
                 <label>Username</label>
-                <input className="bg-white text-black" />
+                <input type="text" value={username} className="bg-white text-black" />
             </div>
             <div className="flex flex-col gap-y-2 w-40">
                 <label>Email address</label>
-                <input className="bg-white text-black" />
+                <input type="email" value={email} className="bg-white text-black" />
             </div>
             <div className="flex flex-col gap-y-2 w-40">
                 <label>Password</label>
-                <input className="bg-white text-black" />
+                <input type="password" value={password} className="bg-white text-black" />
             </div>
         </>
     )
