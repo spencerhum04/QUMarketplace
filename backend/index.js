97a55571fa124088cbaae9a5fcc32212a99e065c
Spencer Hum <83096779+spencerhum04@users.noreply.github.com>
2025-09-01 12:14:40 -0400

    feat: login and signup (#19)

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import userRoute from "./routes/userRoute.js";
 import postRoute from "./routes/postRoute.js";
 import messageRoute from "./routes/messageRoute.js";
+import cors from "cors";
 
 dotenv.config({
     path: ".env"
@@ -16,6 +17,11 @@ const app = express();
 app.use(express.urlencoded ({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true
+}
+app.use(cors(corsOptions));
 
 // api
 app.use("/api/v1/user", userRoute);
